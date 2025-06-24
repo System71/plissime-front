4b9b82aa6f7275620381a7bc95d393aeef681a25
Pierre-Antoine RIGAUDIER <rigaudier.pa@gmail.com>
2025-06-24 22:41:38 +0200

    add paiements

---
+++ src/components/Session/SessionPaid/SessionPaid.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import "./session-paid.css";
+import { format } from "date-fns";
+
+const SessionPaid = ({ title, name, firstName, date, price }) => {
+  const formatedDate = format(date, "dd/LL/yyyy");
+
+  return (
+    <div className="session-paid-item">
+      <div className="session-paid-item-date">{formatedDate}</div>
+      <div className="session-paid-item-name">
+        {name} {firstName}
+      </div>
+      <div className="session-paid-item-title">{title}</div>
+      <div className="session-paid-item-price">{price}Euros</div>
+    </div>
+  );
+};
+
+export default SessionPaid;
--- src/pages/Payments/Payments.jsx
+++ src/pages/Payments/Payments.jsx
@@ -1,9 +1,53 @@
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+import axios from "axios";
+import SessionPaid from "../../components/session/SessionPaid/SessionPaid";
 import "./payments.css";
 
-const Payments = () => {
+const Payments = ({ token }) => {
+  const [sessionsPaid, setSessionsPaid] = useState([]);
+
+  useEffect(() => {
+    if (token) {
+      const fetchSessionsPaid = async () => {
+        try {
+          const response = await axios.get(
+            `${import.meta.env.VITE_API_URL}/sessions/paid`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+              "Content-Type": "multipart/form-data",
+            }
+          );
+          setSessionsPaid(response.data);
+          console.log("reponse.data=", response.data);
+        } catch (error) {
+          console.log(error.response);
+        }
+      };
+      fetchSessionsPaid();
+    }
+  }, [token]);
+
   return (
     <>
-      <h1>PAIEMENTS</h1>
+      <h1>PAIEMENTS RECUS</h1>
+      <div className="session-paid-list">
+        {sessionsPaid.map((session) => {
+          return (
+            <SessionPaid
+              id={session._id}
+              title={session.title}
+              name={session.customer.name}
+              firstName={session.customer.firstName}
+              date={session.start}
+              price={session.price}
+              key={String(session._id)}
+            />
+          );
+        })}
+      </div>
     </>
   );
 };
