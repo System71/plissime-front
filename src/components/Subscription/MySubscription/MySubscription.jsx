dc3fa0e5c5926173125dcb69103d654589f2af00
Pierre-Antoine RIGAUDIER <rigaudier.pa@gmail.com>
2025-12-10 08:26:02 +0100

    backup

--- src/components/Subscription/MySubscription/MySubscription.jsx
+++ src/components/Subscription/MySubscription/MySubscription.jsx
@@ -5,7 +5,8 @@ import axios from "axios";
 import InvoiceItem from "../../InvoiceItem/InvoiceItem";
 
 const MySubscription = ({ token, stripeId }) => {
-  const [invoices, setInvoices] = useState();
+  const [invoices, setInvoices] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,8 +19,8 @@ const MySubscription = ({ token, stripeId }) => {
             },
           }
         );
-        setInvoices(response.data.data);
-        console.log("response.data", response.data.data);
+        setInvoices(response.data);
+        setIsLoading(false);
       } catch (error) {
         console.log("error=", error);
       }
@@ -27,70 +28,73 @@ const MySubscription = ({ token, stripeId }) => {
     fetchData();
   }, [stripeId]);
 
-  return (
-    <div className={styles["my-subscription"]}>
-      <div className={styles["infos"]}>
-        <div className={styles["payment"]}>
-          <h2>Moyen de paiement</h2>
-          <p>CARTE</p>
+  if (!isLoading) {
+    return (
+      <div className={styles["my-subscription"]}>
+        <div className={styles["infos"]}>
+          <div className={styles["payment"]}>
+            <h2>Moyen de paiement</h2>
+            <p>CARTE</p>
+          </div>
+          <div className={styles["invoices"]}>
+            <h2>Factures</h2>
+            <div className={styles["invoices-list"]}>
+              {invoices.map((invoice, index) => (
+                <InvoiceItem
+                  date={invoice.date}
+                  title={invoice.title}
+                  statut={invoice.status}
+                  url={invoice.pdf}
+                  key={index}
+                />
+              ))}
+            </div>
+          </div>
         </div>
-        <div className={styles["invoices"]}>
-          <h2>Factures</h2>
-          <div className={styles["invoices-list"]}>
-            {invoices.map((invoice) => (
-              <InvoiceItem
-                date={invoice.created}
-                title="Abonnement Plissime"
-                statut="Payé"
-                url={invoice.invoice_pdf}
-              />
-            ))}
+        <div className={styles["subscription-block"]}>
+          <div className={styles["title"]}>Premium</div>
+          <div className={styles["item-list"]}>
+            <div className={styles["item"]}>
+              <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
+              <p>Pilotez votre activité</p>
+            </div>
+            <div className={styles["item"]}>
+              <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
+              <p>Prise de rdv en ligne</p>
+            </div>
+            <div className={styles["item"]}>
+              <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
+              <p>Espace client</p>
+            </div>
+            <div className={styles["item"]}>
+              <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
+              <p>Création de programme avec vidéos</p>
+            </div>
+            <div className={styles["item"]}>
+              <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
+              <p>Messagerie</p>
+            </div>
+            <div className={styles["item"]}>
+              <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
+              <p>Dossier client</p>
+            </div>
+            <div className={styles["item"]}>
+              <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
+              <p>Paiement en ligne</p>
+            </div>
+            <div className={styles["item"]}>
+              <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
+              <p>Facturation électronique</p>
+            </div>
+            <div className={styles["item"]}>
+              <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
+              <p>Version mobile</p>
+            </div>
           </div>
         </div>
       </div>
-      <div className={styles["subscription-block"]}>
-        <div className={styles["title"]}>Premium</div>
-        <div className={styles["item-list"]}>
-          <div className={styles["item"]}>
-            <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
-            <p>Pilotez votre activité</p>
-          </div>
-          <div className={styles["item"]}>
-            <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
-            <p>Prise de rdv en ligne</p>
-          </div>
-          <div className={styles["item"]}>
-            <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
-            <p>Espace client</p>
-          </div>
-          <div className={styles["item"]}>
-            <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
-            <p>Création de programme avec vidéos</p>
-          </div>
-          <div className={styles["item"]}>
-            <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
-            <p>Messagerie</p>
-          </div>
-          <div className={styles["item"]}>
-            <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
-            <p>Dossier client</p>
-          </div>
-          <div className={styles["item"]}>
-            <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
-            <p>Paiement en ligne</p>
-          </div>
-          <div className={styles["item"]}>
-            <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
-            <p>Facturation électronique</p>
-          </div>
-          <div className={styles["item"]}>
-            <FontAwesomeIcon icon="check" color="#E67E22" size="xl" />
-            <p>Version mobile</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default MySubscription;
