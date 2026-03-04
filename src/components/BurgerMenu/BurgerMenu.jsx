f6df54b019fc5417ac92fb14707d434221deaa88
Pierre-Antoine RIGAUDIER <rigaudier.pa@gmail.com>
2026-03-04 14:28:10 +0100

    responsive nav menu

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -39,37 +39,67 @@ const BurgerMenu = ({
               <FontAwesomeIcon icon="xmark" color="#E67E22" size="2xl" />
             </div>
             <div className={styles["nav-item"]}>
-              <FontAwesomeIcon icon="chart-line" color="#E67E22" size="xl" />
+              <FontAwesomeIcon
+                icon="chart-line"
+                color="#E67E22"
+                size="xl"
+                fixedWidth
+              />
               <Link to="/" onClick={() => setIsVisible(!isVisible)}>
                 Pilotage
               </Link>
             </div>
             <div className={styles["nav-item"]}>
-              <FontAwesomeIcon icon="user" color="#E67E22" size="xl" />
+              <FontAwesomeIcon
+                icon="user"
+                color="#E67E22"
+                size="xl"
+                fixedWidth
+              />
               <Link to="/customers" onClick={() => setIsVisible(!isVisible)}>
                 Clients
               </Link>
             </div>
             <div className={styles["nav-item"]}>
-              <FontAwesomeIcon icon="calendar-days" color="#E67E22" size="xl" />
+              <FontAwesomeIcon
+                icon="calendar-days"
+                color="#E67E22"
+                size="xl"
+                fixedWidth
+              />
               <Link to="/planning" onClick={() => setIsVisible(!isVisible)}>
                 Agenda
               </Link>
             </div>
             <div className={styles["nav-item"]}>
-              <FontAwesomeIcon icon="square-binary" color="#E67E22" size="xl" />
+              <FontAwesomeIcon
+                icon="square-binary"
+                color="#E67E22"
+                size="xl"
+                fixedWidth
+              />
               <Link to="/programmes" onClick={() => setIsVisible(!isVisible)}>
                 Programmes
               </Link>
             </div>
             <div className={styles["nav-item"]}>
-              <FontAwesomeIcon icon="euro-sign" color="#E67E22" size="xl" />
+              <FontAwesomeIcon
+                icon="euro-sign"
+                color="#E67E22"
+                size="xl"
+                fixedWidth
+              />
               <Link to="/payments" onClick={() => setIsVisible(!isVisible)}>
                 Paiements
               </Link>
             </div>
             <div className={styles["nav-item"]}>
-              <FontAwesomeIcon icon="gear" color="#E67E22" size="xl" />
+              <FontAwesomeIcon
+                icon="gear"
+                color="#E67E22"
+                size="xl"
+                fixedWidth
+              />
               <Link
                 to="/user/settings"
                 onClick={() => setIsVisible(!isVisible)}
@@ -78,7 +108,12 @@ const BurgerMenu = ({
               </Link>
             </div>
             <div className={styles["nav-item"]}>
-              <FontAwesomeIcon icon="question" color="#E67E22" size="xl" />
+              <FontAwesomeIcon
+                icon="question"
+                color="#E67E22"
+                size="xl"
+                fixedWidth
+              />
               <Link to="/help" onClick={() => setIsVisible(!isVisible)}>
                 Aide
               </Link>
@@ -94,6 +129,118 @@ const BurgerMenu = ({
                   setSub("");
                   setFirstName("");
                   setStripeId("");
+                  setSessionsList([]);
+                  setCustomersList([]);
+                  navigate("/");
+                }}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  } else if (role === "customer") {
+    return (
+      <div className={styles["burger-menu"]}>
+        {/* <img src={logo} alt="logo-plissime" className={styles["logo-nav"]} /> */}
+        <div
+          className={styles["toggle"]}
+          onClick={() => setIsVisible(!isVisible)}
+        >
+          <FontAwesomeIcon icon="bars" color="#E67E22" size="2xl" />
+        </div>
+        {isVisible && (
+          <div className={styles["menu"]}>
+            <div
+              className={styles["cross"]}
+              onClick={() => setIsVisible(!isVisible)}
+            >
+              <FontAwesomeIcon icon="xmark" color="#E67E22" size="2xl" />
+            </div>
+            <div className={styles["nav-item"]}>
+              <FontAwesomeIcon
+                icon="chart-line"
+                color="#E67E22"
+                size="xl"
+                fixedWidth
+              />
+              <Link
+                to="/"
+                className={styles.picto}
+                onClick={() => setIsVisible(!isVisible)}
+              >
+                Accueil
+              </Link>
+            </div>
+            <div className={styles["nav-item"]}>
+              <FontAwesomeIcon
+                icon="user"
+                color="#E67E22"
+                size="xl"
+                fixedWidth
+              />
+              <Link
+                to="/mycoachs"
+                className={styles.picto}
+                onClick={() => setIsVisible(!isVisible)}
+              >
+                Mes coachs
+              </Link>
+            </div>
+            <div className={styles["nav-item"]}>
+              <FontAwesomeIcon
+                icon="square-binary"
+                color="#E67E22"
+                size="xl"
+                fixedWidth
+              />
+              <Link
+                to="/myprograms"
+                className={styles.picto}
+                onClick={() => setIsVisible(!isVisible)}
+              >
+                Mes programmes
+              </Link>
+            </div>
+            <div className={styles["nav-item"]}>
+              <FontAwesomeIcon
+                icon="gear"
+                color="#E67E22"
+                size="xl"
+                fixedWidth
+              />
+              <Link
+                to="/customer/settings"
+                className={styles.picto}
+                onClick={() => setIsVisible(!isVisible)}
+              >
+                Mes informations
+              </Link>
+            </div>
+            <div className={styles["nav-item"]}>
+              <FontAwesomeIcon
+                icon="question"
+                color="#E67E22"
+                size="xl"
+                fixedWidth
+              />
+              <Link
+                to="/help"
+                className={styles.picto}
+                onClick={() => setIsVisible(!isVisible)}
+              >
+                Aide
+              </Link>
+            </div>
+            <div className={styles["disconnect"]}>
+              <Button
+                text="Déconnexion"
+                action={() => {
+                  Cookies.remove("plissimeToken");
+                  localStorage.removeItem("role");
+                  setToken("");
+                  setRole(null);
+                  setFirstName("");
                   setSessionsList([]);
                   setCustomersList([]);
                   navigate("/");
